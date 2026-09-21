1d9850f3125cbb6c9269bf16fcd8b26e54fbbb3b
Md Samsul Alam <samsul72b@gmail.com>
2026-09-21 13:56:37 +0600

    Update StoringNumber.cpp

--- Day 3/YourWork/StoringNumber.cpp
+++ Day 3/YourWork/StoringNumber.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     int n;
     cin >> n;
-    
     vector<int> v;
     for(int i = 0; i < n; i++) {
         int x;
@@ -12,8 +10,7 @@ int main() {
         v.push_back(x); 
     for (int i=0;i < n;i++)
     {
-			    cout << v[i] << endl;
+	cout << v[i] << endl;
     }
-    
     return 0;
 }
